63264298b0d5f889e37ef4e84b14a7583010106a
Vítor Conti <vitorconti@Vitors-MacBook-Pro-2.local>
2022-08-31 16:08:53 -0300

    feat: added token functions

--- src/Compiler/LexicalAnalyser/LexicalAnalyser.java
+++ src/Compiler/LexicalAnalyser/LexicalAnalyser.java
@@ -2,14 +2,16 @@ package Compiler.LexicalAnalyser;
 
 import Services.FileReader;
 
-import java.lang.reflect.Array;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LexicalAnalyser {
   ArrayList<String> reservedSymbols = new ArrayList<String>();
   ArrayList<String> reservedLexemes = new ArrayList<String>();
   ArrayList<String> codeToAnalyse = new ArrayList<String>();
   ArrayList<String[]> tokenArray = new ArrayList<String[]>();
+  public int indexStopped = 0;
 
   public LexicalAnalyser() {
     FileReader reader = new FileReader();
@@ -60,4 +62,85 @@ public class LexicalAnalyser {
 
     return texto;
   }
+
+  public String[] treatDigit (String wordToTreat) {
+    String digit = "";
+    for(int i=0; i< wordToTreat.length(); i++){
+      if(Character.isDigit(wordToTreat.charAt(i))){
+        digit += wordToTreat.charAt(i);
+      } else  {
+        this.indexStopped = i;
+        break;
+      }
+    }
+
+    String[] values = {digit, "snumero"};
+
+    return values;
+  }
+
+  public String[] treatIdentifier (String wordToTreat) {
+    String word = "";
+    for(int i=0; i< wordToTreat.length(); i++){
+      char character = wordToTreat.charAt(i);
+      if(Character.isDigit(character) || Character.isAlphabetic(character) || character == '_'){
+        word += wordToTreat.charAt(i);
+      } else {
+        this.indexStopped = i;
+        break;
+      }
+    }
+
+    String lexeme = searchReserved(word);
+    String[] values = {word, lexeme};
+
+    return values;
+  }
+
+  public String[] treatAttributions (String wordToTreat) {
+    String word = "";
+    char character = wordToTreat.charAt(1);
+    if(character == '='){
+      String[] values = {":=", "satribuição"};
+
+      return values;
+    }
+
+    return null;
+  }
+
+  public String[] treatOperations (String wordToTreat) {
+    String word = "";
+    char character = wordToTreat.charAt(0);
+    if(character == '+' || character == '-' || character == '*'){
+      String[] values = {Character.toString(character), searchReserved(Character.toString(character))};
+
+      return values;
+    }
+
+    return null;
+  }
+
+  public String[] treatPontuation (String wordToTreat) {
+    String word = "";
+    char character = wordToTreat.charAt(0);
+    if(character == ';' || character == '(' || character == ')' || character == ')'){
+      String[] values = {Character.toString(character), searchReserved(Character.toString(character))};
+
+      return values;
+    }
+
+    return null;
+  }
+
+  public String searchReserved(String word) {
+    int index = reservedLexemes.indexOf(word);
+    if(index >= 0){
+      return reservedSymbols.get(index);
+    }
+
+    return "sidentificador";
+  }
+
+
 }
